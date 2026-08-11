991e90b5d55127f7861a6b795bafbba7b572e74a
Kai Manyeh <raventrack@Kais-MacBook-Pro-2.local>
2026-08-11 22:24:25 +0100

    deploy: 2026-08-11 21:24 UTC

--- gen_city_pages.py
+++ gen_city_pages.py
@@ -246,8 +246,8 @@ def make_city_page(c):
 }}
 </script>
 
-<link rel="icon" type="image/x-icon" href="/favicon.ico?v=2">
-<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png?v=2">
+<link rel="icon" type="image/x-icon" href="/favicon.ico?v=3">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png?v=3">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png?v=2">
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png?v=3">
 <link rel="preload" href="{ap}assets/shared.css" as="style">
